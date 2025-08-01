951dc974d0395ae0cc1c1b1a8ec413ad54b852ac
muzammil206 <ismailmuzammil206@gmail.com>
2025-08-01 17:54:38 +0100

    fixed

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import DashboardPage from "./dashboard/page";
+import  SignupPage from "./singnUp/page";
 
 export default function Home() {
   return (
     <div>
-      <DashboardPage />
+      <SignupPage />
     </div>
   );
 }
